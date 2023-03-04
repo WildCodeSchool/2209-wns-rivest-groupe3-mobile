649bab6c3fd2261197c1704fe949e4c9c10d168d
HolmesEH <s.holmes.eh@gmail.com>
2023-03-04 16:16:10 +0100

    :sparkles: feat(e09-t09) implement dark/light mode themes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
-import RootNavigator from './src/navigation'
+import BottomTabs from './src/navigation/Tabs'
+import { TabasColorTheme } from './src/interfaces'
+
+const TabasLightTheme: TabasColorTheme = {
+  dark: false,
+  colors: {
+    primary: '#0B2534',
+    background: '#f8f8f2',
+    highlight: '#34A0B4',
+    card: 'rgb(255, 255, 255)',
+    text: '#0B2534',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+}
+const TabasDarkTheme: TabasColorTheme = {
+  dark: true,
+  colors: {
+    primary: 'rgb(255, 45, 85)',
+    highlight: '#34A0B4',
+    background: '#0B2534',
+    card: 'rgb(255, 255, 255)',
+    text: '#f8f8f2',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+}
 
 export default function App() {
+  const scheme = useColorScheme()
+
   return (
-    <>
-      <RootNavigator />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer
+      theme={scheme === 'dark' ? TabasDarkTheme : TabasLightTheme}
+    >
+      <BottomTabs />
+    </NavigationContainer>
   )
 }
---
+++ src/interfaces.tsx
@@ -0,0 +1,7 @@
+import type { Theme } from '@react-navigation/native'
+
+export type TabasColorTheme = {
+  colors: {
+    highlight: string
+  }
+} & Theme
