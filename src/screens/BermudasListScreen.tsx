c63f4f79b5d6ab93297d8cc93f22922b3579960e
Antonia <antoniatch@zohomail.eu>
2023-06-23 16:04:03 +0200

    image picker

--- src/screens/BermudasListScreen.tsx
+++ src/screens/BermudasListScreen.tsx
@@ -1,7 +1,25 @@
-import { View } from 'react-native'
+import { useTheme } from '@react-navigation/native'
+import { View, Text, StyleSheet } from 'react-native'
+import { TabasColorTheme } from '../interfaces'
 
 const BermudasListScreen = () => {
-  return <View>Bermudas</View>
+  const { colors } = useTheme() as TabasColorTheme
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: colors.text, fontSize: 20, fontWeight: 'bold' }}>
+        Bermudas
+      </Text>
+    </View>
+  )
 }
 
 export default BermudasListScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
--- src/screens/BermudasScreen.tsx
+++ src/screens/BermudasScreen.tsx
@@ -1,24 +1,111 @@
+import { useState, useContext, useRef } from 'react'
 import { useTheme } from '@react-navigation/native'
-import { View, Text, StyleSheet } from 'react-native'
-import { TabasColorTheme } from '../interfaces'
+import * as ImagePicker from 'expo-image-picker'
 
-const BermudasScreen = () => {
-  const { colors } = useTheme() as TabasColorTheme
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { Video, ResizeMode } from 'expo-av'
+
+import { TabasColorTheme } from '../interfaces'
+import { BermudasContext } from '../contexts/BermudasContext'
+
+const BermudasScreen = ({ navigation }: any) => {
+  const { colors, fonts } = useTheme() as TabasColorTheme
+
+  const { localImage, setLocalImage, localVideo, setLocalVideo } =
+    useContext(BermudasContext)
+
+  const [cloudinaryUrl, setCloudinaryUrl] = useState<string | null>(null)
+
+  const [libraryStatus, requestLibraryPermission] =
+    ImagePicker.useMediaLibraryPermissions()
+
+  const pickImage = async () => {
+    if (!libraryStatus?.granted) {
+      requestLibraryPermission()
+    }
+
+    let media = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.7,
+      base64: true,
+      videoMaxDuration: 30,
+    })
+
+    if (!media?.canceled) {
+      if (media.assets[0].type === 'image') {
+        setLocalVideo(null)
+        setLocalImage(media.assets[0])
+      }
+      if (media.assets[0].type === 'video') {
+        setLocalVideo(media.assets[0])
+        setLocalImage(null)
+      }
+    }
+  }
+
+  // const data = new FormData();
+  //   data.append('fileData, {
+  //     uri: image,
+  //     type: 'image/jpeg',
+  //     name: 'blabla,
+  //   });
+
+  // let base64Img = data:image/jpg;base64,${pickerResult.base64};
+
+  // JSON.stringify()
+
+  // await singleFileUploader({
+  //   distantUrl:
+  //     "https://wildstagram.nausicaa.wilders.dev/upload",
+  //   expectedStatusCode: 201,
+  //   filename: image.item,
+  //   filetype: "image/jpeg",
+  //   formDataName: "fileData",
+  //   localUri:
+  //     FileSystem.cacheDirectory +
+  //     "ImageManipulator/" +
+  //     image.item,
+  //   token: Constants.manifest.extra.token,
+  // });
+
   return (
-    <View style={styles.container}>
-      <Text style={{ color: colors.text, fontSize: 20, fontWeight: 'bold' }}>
-        Bermudas Screen
-      </Text>
+    <View style={main.container}>
+      <TouchableOpacity onPress={pickImage}>
+        <Text>Sélectionner un bermuda dans la galerie</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
+        <Text>Prendre un bermuda</Text>
+      </TouchableOpacity>
+
+      {localImage && (
+        <Image style={main.media} source={{ uri: localImage?.uri }} />
+      )}
+      {localVideo && (
+        <Video
+          style={main.media}
+          source={{ uri: localVideo.uri }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+        />
+      )}
     </View>
   )
 }
 
 export default BermudasScreen
 
-const styles = StyleSheet.create({
+const main = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  media: {
+    flex: 0.5,
+    width: '80%',
+    margin: 20,
   },
 })
